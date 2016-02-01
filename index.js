d061bada18c63174a0514905387a424b66d65acc
mike <mike442144@163.com>
2016-02-02 00:30:02 +0800

    first version with worker and client.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
+"use strict";
+
 /**
  * Module dependencies.
  */
 
-var program = require('commander');
+const program = require('commander');
+let App = null, app=null
 
 program
     .version('0.0.1')
     .option('-c, --client', 'run as client')
     .option('-w, --worker', 'run as worker')
+    .option('-r, --recover','recover mode, read seed from database')
     .option('-n, --name<name>','app to run')
     .parse(process.argv);
 
@@ -18,10 +22,14 @@ console.log('run floodesh as:');
 if (program.client) {
     console.log('  - client');
     // do client stuff
+    App = require("./lib/client.js");
 }
 
 if (program.worker){
     console.log('  - worker');
     // do worker stuff
+    App = require("./lib/worker.js")
 }
 
+app = new App()
+app.start()
---
+++ tests/test.worker.js
@@ -0,0 +1,62 @@
+
+"use strict"
+
+const should = require('should')
+
+function testspider(){
+    this.name = "testspider";
+}
+
+testspider.prototype = {
+    constructor:testspider,
+    
+    parse:function(ctx,done){
+	let $ = ctx.$;
+	console.log("parsing...");
+	let tit = $("title").text().trim();
+	ctx.dataSet.set("title",tit);
+	ctx.tasks.push({opt:{uri:"http://www.163.com"},next:"parseNETEASE"});
+	
+	done();
+    },
+
+    parseNETEASE:function(ctx,done){
+	console.log(ctx.$("title").text());
+	
+	done();
+    }
+}
+
+/// should be invoked in app.js file in each project.
+const Worker = require("../lib/worker.js")
+
+/* 
+ *  attach `testspider` instance to `Worker` instance
+ *  
+ */
+let config = {
+    gearman:{
+	"servers":[{"host":"192.168.98.116"}]
+    },
+    schedule:{
+	concurrent: 10,
+	rate: 1000 // ms between every request
+    }
+};
+
+let worker = new Worker(config);
+const cheerio = require("cheerio");
+
+worker.responsemw.use( (ctx,next) => {
+    ctx.content = ctx.body.toString();
+    return next();
+})
+
+worker.responsemw.use( (ctx,next) => {
+    ctx.$= cheerio.load(ctx.content);
+    return next();
+})
+
+worker.parsedmw.use( ctx => console.log("new task number: %d",ctx.tasks.length))
+
+worker.attach(new testspider()).start();
