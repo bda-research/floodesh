4bafa1f0812e2e3b15ebbc62e3e9719c0db87c0a
mike <mike442144@163.com>
2016-02-25 20:28:33 +0800

    0.0.4

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/* 
+ * Expose Worker and Client AS API
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+module.exports = {
+    Worker:require('./lib/worker.js'),
+    Client:require('./lib/client.js')
+}
