471282a28651a2df360f329bc2cb38dba07dc980
mike <mike442144@163.com>
2016-05-13 20:55:14 +0800

    quick fix console

--- templates/test/test-spider.js
+++ templates/test/test-spider.js
@@ -3,11 +3,14 @@
 
 const should = require('should')
 const sinon = require('sinon')
+const cheerio = require('cheerio')
+const Spider = require('../lib/spider.js')
+
 require('should-sinon')
 
 describe('Spider',function(){
+    let ctx={dataSet:new Map(), tasks:[]};
     beforeEach( () => {//do something before each test case.
-	
     });
     
     describe('#constructor', () => {//test spider constructor
