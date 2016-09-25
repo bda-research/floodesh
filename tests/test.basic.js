880ee358743c8f81aa1f61713fba31639b037f51
Mike Chen <mike442144@hotmail.com>
2016-09-25 22:44:58 +0800

    testing new worker

--- tests/test.basic.js
+++ tests/test.basic.js
@@ -1,74 +1,43 @@
-var WC = require('../index.js')
-var wc = new WC()
-wc.use('request','/',function(err,req,res,next){
-    if(err){
-	console.log(err);
-	//return;
-    }
-    
-    console.log("everything on request.");
 
-    next();
+"use strict"
+
+const should = require('should')
+const path = require('path')
+const sinon = require('sinon')
+require('should-sinon')
+const Worker = require('../worker')
+
+describe('worker', ()=>{
+    beforeEach(() => {
+	
+    });
+
+    it("should new a worker", ()=> {
+	process.chdir(path.join(process.cwd(),'tests'));
+	let options = {functions:['parse', 'parseDetail']};
+	let worker = new Worker(options);
+	
+	worker.use((ctx, next) => {
+	    console.log(1);
+	    return next();
+	});
+	
+	worker.use((ctx, next) => {
+	    console.log(2);
+	    return next();
+	});
+	
+	worker.use((ctx, next) => {
+	    console.log(3);
+	    return next();
+	});
+	
+	worker.use((ctx, next) => {
+	    console.log(4);
+	    return next();
+	});
+
+	//worker.start();
+	//worker._onJob('parse')({payload:'{"uri":"http://www.baidu.com"}'});
+    })
 });
-
-wc.use('request',function(err,req,res,next){
-    if(err){
-	console.log(err);
-    }
-    
-    console.log("everything no route of request");
-    
-    next();
-});
-
-wc.use('request','/q',function(err,req,res,next){
-    if(err){
-	console.log(err);
-    }
-    console.log("request /q");
-    next();
-});
-
-
-wc.use('process','/',function(err,req,res,next){
-    if(err){
-	console.log(err);
-    }
-
-    console.log("everything on processing");
-
-    next();
-});
-
-wc.use('process','/bbc',function(err,req,res,next){
-    if(err){
-	console.log(err);
-    }
-
-    console.log("bbc on processing");
-
-    next();
-});
-
-wc.use('processed','/',function(err,req,res,next){
-    if(err){
-	console.log(err);
-    }
-
-    console.log("everything on processed");
-
-    next();
-});
-
-
-// wc._handle('request',{uri:'http://www.baidu.com/'},{},function(){
-//     console.log("request end");
-// });
-
-// wc._handle('request',{uri:'http://www.baidu.com/q'},{},function(){
-//     console.log('request /q done');
-// })
-
-wc._handle('process',{uri:'http://www.baidu.com/bbc'},{},function(){
-    console.log("process end");
-})
--- worker/index.js
+++ worker/index.js
@@ -5,14 +5,18 @@ const Core = require('floodesh-lib')
 const gearman = require("gearman-node-bda")
 const functionsIn = require("lodash/functionsIn")
 const debug = require("debug")("floodesh-worker")
+const path = require('path')
 
-module.exports = class Worker extends Core{
-    constructor(config){
+
+
+module.exports = class Worker extends Core {
+    constructor(options){
 	super();
-	this.config = config;
+	this.config = require(path.join(process.cwd(),'config'))
 	delete this.config.gearman.loadBalancing;
-
-	return this;
+	
+	this._init();
+	this._registerParser(options.functions);
     }
     
     /*
@@ -66,10 +70,18 @@ module.exports = class Worker extends Core{
      */
     _onJob(fname){
 	let self = this;
+	this.use((ctx, next) => {
+	    ctx.parser = fname;
+	    console.log("middleware: %s", fname);
+	    return next();
+	});
+	
 	return function(job){
 	    self._job = job;
 	    let opt = self._parseJobArgv();
-	    opt.parse = self.app[fname]; //bind parser provided by spider, default for `parser`
+	    //opt.parse = self.app[fname]; //bind parser provided by spider, default for `parser`
+	    
+	    
 	    self.enqueue(opt);// notify core to schedule the `options`
 	}
     }
@@ -95,6 +107,25 @@ module.exports = class Worker extends Core{
 	    self._w.close();
 	});
     }
+
+    _register2Server(){
+	return {
+	    addFunction:(name,fn)=>{
+		console.log("add function: %s",name);
+	    }
+	}
+	//return gearman.worker(this.config.gearman);
+    }
+
+    _registerParser(names){
+	if(!(names instanceof Array)){
+	    names = [names];
+	}
+	
+	for(let i=0;i<names.length;i++){
+	    this._w.addFunction('app name_'+names[i], this._onJob(names[i]));
+	}
+    }
     
     /* Initilize Gearman Worker, 
      *  register functions, initialize scheduler and subscribe events of `Core`
@@ -102,18 +133,19 @@ module.exports = class Worker extends Core{
      * @api private
      */
     _init(){
-	this._w = gearman.worker(this.config.gearman)//register worker to master
+	this._w = this._register2Server()//gearman.worker(this.config.gearman)//register worker to master
 	this.retryPriority = 10;
 	this._hookSIG();
+
 	//this.initializeScheduler(this.config.schedule);
 	
-	functionsIn(this.app).forEach( fnKey => this._w.addFunction(this.app.name+"_"+fnKey, this._onJob(fnKey) ), this);//bind functions
+	//functionsIn(this.app).forEach( fnKey => this._w.addFunction(this.app.name+"_"+fnKey, this._onJob(fnKey) ), this);//bind functions
         
-	this.on("request", this._send.bind(this) )
-	    .on("response", this._parse.bind(this) )
-	    .on("process", this._back.bind(this) )
-	    .on('error.middleware', this._onError.bind(this))
-	    .on('error.request',this._onError.bind(this) );
+	this.on("complete", this._back.bind(this) )
+	    .on('error', this._onError.bind(this))
+//	    .on("request", this._send.bind(this) )
+//	    .on("response", this._parse.bind(this) )
+//	    .on('error.request',this._onError.bind(this) );
 
 	// let self = this;
 	// this._w.on("socketError",(id, e)=>{console.error("%s, %s",id, e.stack)});
@@ -134,6 +166,10 @@ module.exports = class Worker extends Core{
 	}
 	
 	(!this._w.closed) && this._job.workComplete( JSON.stringify([]) );
+    }
+
+    get job(){
+	return this._job;
     }
     
     /* Attach customer's app which should subscribe events: `request`, `response`, `process`
